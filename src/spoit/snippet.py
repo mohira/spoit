55a4a21bd6b8ee199ece2abc4eef46b0112c461f
mohira <shukatsudaison@gmail.com>
2022-05-01 17:10:28 +0900

    refactor: build_snippet

--- src/spoit/snippet.py
+++ src/spoit/snippet.py
@@ -1,26 +1,22 @@
 import pandas as pd
 
+template = """usecols = [
+{body}
+]
+df = pd.read_csv('{filename}', usecols=usecols)
+"""
+
 
 def build_snippet(cols: pd.Index, usecols_or_not: list[bool], filename: str, omit: False) -> str:
-    snippet = 'usecols = [\n'
-
-    if omit:
-        usecols = cols[usecols_or_not]
-        each_cols = [f"\t'{col}'," for col in usecols]
-    else:
-        each_cols = []
-        for idx, use in enumerate(usecols_or_not):
-            col = cols[idx]
-            if use:
-                each_cols.append(f"\t'{col}',")
-            else:
-                each_cols.append(f"\t# '{col}',")
+    each_cols = _each_cols(cols, usecols_or_not, omit)
 
     body = '\n'.join(each_cols)
 
-    snippet += body
-    snippet += '\n]\n'
+    return template.format(body=body, filename=filename)
 
-    snippet += f"df = pd.read_csv('{filename}', usecols=usecols)\n"
 
-    return snippet
+def _each_cols(cols: pd.Index, usecols_or_not: list[bool], omit: bool) -> list[str]:
+    if omit:
+        return [f"\t'{col}'," for col in cols[usecols_or_not]]
+
+    return [f"\t'{col}'," if use else f"\t# '{col}'," for use, col in zip(usecols_or_not, cols)]
